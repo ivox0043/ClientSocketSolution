17dcdab6888dcedf359659f84c04ba023edc7dc2
13 13 <egzigo.sunrise@gmail.com>
2017-09-06 11:09:08 +0200

    Add project files.

---
+++ ClientSocketSOlution/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+
+namespace ClientSocket
+{
+    class MainProgramClient
+    {
+        static void Main(string[] args)
+        {
+
+            TcpClient server = new TcpClient("10.140.70.21", 20001);
+            string messageFromServer;
+            string messageToServer;
+            bool isConnected;
+
+            //Trying to connect to server and creating connection
+            try
+            {
+
+                NetworkStream stream = server.GetStream();
+                StreamReader reader = new StreamReader(stream);
+                StreamWriter writer = new StreamWriter(stream);
+                isConnected = true;
+                
+
+                while (isConnected)
+                {
+                    messageFromServer = reader.ReadLine();
+                    Console.WriteLine(messageFromServer);
+                    messageToServer = Console.ReadLine();
+                    writer.WriteLine(messageToServer);
+                    writer.Flush();
+                    
+                                
+                }
+
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("fuck");
+
+            }
+
+        }
+    }
+}
